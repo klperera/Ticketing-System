6eba2ee0a5e60b8c2f12320ff69b8dd1f54a5bf4
klperera <klperera@users.noreply.github.com>
2024-11-01 20:58:43 +0530

    Initial commit

---
+++ src/main/java/com/OOP/CW/TicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.OOP.CW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketingSystemApplication.class, args);
+	}
+
+}
