bcf730e9e6211703150a1f74a13969026534afeb
klperera <klperera@users.noreply.github.com>
2024-12-06 17:32:24 +0530

    navbar drop down

--- src/main/TicketSystemGUI/src/app/nav-bar/nav-bar.component.ts
+++ src/main/TicketSystemGUI/src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [NgClass,],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  isDropDownOpen = false;
 
+  toggleDropDown(){
+    this.isDropDownOpen = !this.isDropDownOpen;  
+  }
 }
