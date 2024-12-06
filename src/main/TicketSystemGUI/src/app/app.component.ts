c6221572879f8bb124e5bec050a25ac163d8a71b
klperera <klperera@users.noreply.github.com>
2024-12-06 21:02:42 +0530

    welcome page design

--- src/main/TicketSystemGUI/src/app/app.component.ts
+++ src/main/TicketSystemGUI/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, WelcomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
