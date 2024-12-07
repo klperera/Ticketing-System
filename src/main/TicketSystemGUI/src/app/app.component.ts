f8a7e3d4d311c174b643f7a2d9f410cf0d9f2919
klperera <klperera@users.noreply.github.com>
2024-12-07 11:57:30 +0530

    register update

--- src/main/TicketSystemGUI/src/app/app.component.ts
+++ src/main/TicketSystemGUI/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { RegisterComponent } from './register/register.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, NavBarComponent, WelcomePageComponent, RegisterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
