d2b7883a662ec6707e5af638fa9f2fef3267674b
klperera <klperera@users.noreply.github.com>
2024-12-07 19:53:21 +0530

    app routing and config

--- src/main/TicketSystemGUI/src/app/app.routes.ts
+++ src/main/TicketSystemGUI/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'welcomePage', pathMatch: 'full'},
     {path: 'welcomePage', component: WelcomePageComponent},
     {path: 'signUp', component: RegisterComponent},
-    {path: 'signIn', component: LoginComponent}
-    // {path: 'vendornav', component: vendorComponent}
-
+    {path: 'signIn', component: LoginComponent},
+    {path: 'home', component: HomePageComponent}
 ];
