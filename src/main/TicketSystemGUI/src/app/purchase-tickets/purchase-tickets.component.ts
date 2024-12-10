0c64460aed699ea3773ce39146093872525073f0
klperera <klperera@users.noreply.github.com>
2024-12-10 12:40:36 +0530

    purchase tickets basic component

---
+++ src/main/TicketSystemGUI/src/app/purchase-tickets/purchase-tickets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-purchase-tickets',
+  standalone: true,
+  imports: [],
+  templateUrl: './purchase-tickets.component.html',
+  styleUrl: './purchase-tickets.component.css'
+})
+export class PurchaseTicketsComponent {
+
+}
