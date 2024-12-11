db48ec884b9c9bccc032bd07b05e723cc0c201a1
klperera <klperera@users.noreply.github.com>
2024-12-11 17:38:56 +0530

    basic all event customer

---
+++ src/main/TicketSystemGUI/src/app/all-event-customer/all-event-customer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-event-customer',
+  standalone: true,
+  imports: [],
+  templateUrl: './all-event-customer.component.html',
+  styleUrl: './all-event-customer.component.css'
+})
+export class AllEventCustomerComponent {
+
+}
