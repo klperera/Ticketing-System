19471ca73ad6ebf7fb35fc7aa9a081215c562e9e
kalpa Perera <klperera660@gmail.com>
2025-06-05 14:07:02 +0530

    testing setup

--- src/test/java/com/OOP/CW/TicketingSystemApplicationTests.java
+++ src/test/java/com/OOP/CW/TicketingSystemApplicationTests.java
@@ -1,9 +1,10 @@
 package com.OOP.CW;
 
+import com.OOP.CW.Backend.TicketingSystemApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = TicketingSystemApplication.class)
 class TicketingSystemApplicationTests {
 
 	@Test
